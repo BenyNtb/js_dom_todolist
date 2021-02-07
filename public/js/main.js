9d0bd6c928444177865ec36761553328d52a590a
Beny <b.ntambwe97@gmail.com>
2021-02-07 20:11:08 +0100

    modification, html,css et js, import-export fait, bouton modif non fonctionnelle

--- public/js/main.js
+++ public/js/main.js
@@ -1,81 +1,54 @@
-let body = document.body
-console.log(body);
-let taskName = document.querySelector('input')
-let addButton = document.querySelectorAll('button')[0]
-let allButton = document.querySelectorAll('button')[1]
-let doneButton = document.querySelectorAll('button')[2]
-let todoButton = document.querySelectorAll('button')[3]
-let listeUL1 = document.getElementById('Liste1')
-let listeUL2 = document.getElementById('Liste2')
-let listeUL3 = document.getElementById('Liste3')
-let listeUL4 = document.getElementById('Liste4')
-let contenu= document.querySelector('ul#liste')
-let monTabDone = []
-let monTabtodo = []
-let monTabAll = []
+import {myBtn, myInp, myList} from "./Events.js"
 
-console.log(taskName);
-console.log(addButton);
-console.log(allButton);
-console.log(doneButton);
-console.log(todoButton);
-// console.log(liste);
-// EVENT
-// document.addEventListener("keyup", function(event){
-//     if (event.key == 13){
-//         let todoButton = taskName;
-//         if(toDo){
-//             addButton(toDo)
-//         }
-//         taskName.value = "";
-//     }
-// })
-// addButton.addEventListener('click', () => {
-//     listeUL.appendChild(listeUL.firstChild)
-// })
-
-// ADD A TASK
-
-let monTab = []
-
-addButton.onclick = () => {
-    let userValue = taskName.value;// recup val.input
-if (userValue === userValue) {
-        let monLi = document.createElement('li');
-        monTab.push(userValue);
-        listeUL1.appendChild(monLi); 
-        // SPAN
-        let span = document.createElement('span');
-        span.style.backgroundColor = 'white';
-        span.style.padding = '0 200px';
-        span.style.margin = '10px 20px';
-        span.innerText = userValue;
-        monLi.append(span);
-        // CHECKBOX
-        let checkBox = document.createElement('input');
-        checkBox.setAttribute("type", "checkbox");
-        checkBox.setAttribute("class", "checkbox");
-        checkBox.style.margin = '10px 20px';
-        monLi.append(checkBox);
-        // BOUTON SUPPRIMER
-        let suppr = document.createElement('button');
-        suppr.setAttribute('color', 'red');
-        suppr.style.margin = '10px 20px';
-        suppr.innerHTML = '<i class="trash fas fa-trash-alt"></i>';
-        monLi.append(suppr)
-        monTab.push(suppr)
-        // BOUTON MODIFIER
+// fonction 
+let addTodo = (e) => {
+    if (e.keyCode == 13) {
+        // Ma div
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('todo')
+        // Ma liste
+        let newList = document.createElement('li')
+        newList.innerText = myInp.value
+        newList.classList.add('todo-item')
+        newDiv.appendChild(newList)
+        // bouton de check
+        let checkBtn = document.createElement('button');
+        checkBtn.innerHTML = '<i class="fas fa-check"></i>';
+        checkBtn.classList.add('check-btn')
+        newDiv.appendChild(checkBtn)
+        // bouton de supprimer
+        let suprBtn = document.createElement('button');
+        suprBtn.innerHTML = '<i class="fas fa-trash"></i>';
+        suprBtn.classList.add('supr-btn')
+        newDiv.appendChild(suprBtn)
+        // bouton modif
         let modif = document.createElement('button');
-        modif.setAttribute('color', 'white')
+        modif.classList.add('modifTache')
         modif.innerHTML = '<i class="pen fas fa-edit"></i>'
-        monLi.append(modif);
-        monTab.push(modif)
-        // Supprime event
-        suppr.addEventListener('click', () => {
-        monLi.remove(suppr.parentNode)
-        })
-        // Modifie event
-        modif.addEventListener('click', () => {
+        newDiv.appendChild(modif);
+        // liste 
+        myList.appendChild(newDiv)
+        // effacer ce qui est dans l'input
+        myInp.value = ''
+    }
+}
+
+// fonction pour supprimer et verifier
+
+let supprCheck = (e) => {
+    let btn = e.target;
+    if (btn.classList[0] === 'supr-btn') {
+        let parent = btn.parentElement;
+        parent.remove();
+    }
+
+    // check
+    if (btn.classList[0] === 'check-btn') {
+        let parent = btn.parentElement;
+        parent.classList.toggle('done');
+    }
+
+    if (btn.classList[0] === 'modifTache') {
             let span2 = span;
             let span2text = span.innerHTML;
             let parent = span2.parentNode;
@@ -89,69 +62,56 @@ if (userValue === userValue) {
             input2M.addEventListener('keyup', (e) => {
                 if (e.keyCode === 13) {
                     let valeurI = input2.value;
-                    let span3 = document.createElement('span');
-                    span3.innerText = valeurI;
-                    parent.replaceChild(span3, input2); 
+                    let span2 = document.createElement('span');
+                    span2.innerText = valeurI;
+                    parent.replaceChild(span2, input2); 
                 }
             })
-        })
-        let checkbox = document.getElementsByClassName('checkbox')
-        Array.from(checkbox).forEach(element => {
-            element.addEventListener('change', (event) => {
-                console.log(element.checked);
-                if (element.checked) {
-                    monTabDone.push(element)
-                    alert('checked');
-                } else {
-                    monTabtodo.push()
-                }
-            })
-            
-        });
     }
-};
-
-// monTabDone.addEventListener('click', (e) => {
-//     if (e.checked) {
-//         console.log(monTabDone);
-//     } else {
-//         console.log(monTabtodo);
-//     }
-// })
-
-
-
-doneButton.onclick = () => {
-    monTabDone.push(listeUL2)
-    // listeUL1.push()
-    console.log(listeUL2);
-    
-    
-    
-    
-    console.log(monTab);
 }
 
-// TASK TO DO
 
 
 
+let filterTodo = (e) => {
+    console.log("hinne" + e.path[0].innerHTML);
+    let todos = Array.from(myBtn.children);
+    let list = Array.from(myList.children);
+    console.log(todos);
+    console.log(list);
+    console.log("length");
+    console.log(list.length);
+    for (let i = 0; i < list.length; i++) {
+        console.log(i);
+        console.log("boucle");
+        console.log(todos[i].innerText);
+        //switch (todos[i].innerText) {
+        switch (e.path[0].innerHTML){
+            case "All":
+                console.log('test');
+                list[i].style.display = "flex";
+                break;
+            case "Done":
+                if (list[i].classList.contains('done')) {
+                    list[i].style.display = "flex";
+                } else {
+                    list[i].style.display = "none";
+                }
+                break;
+            case "To Do":
+                if (!list[i].classList.contains('done')) {
+                    list[i].style.display = "flex";
+                } else {
+                    list[i].style.display = "none";
+                }
+                break;
+        }
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// EVENT 
+myInp.addEventListener('keypress', addTodo);
+myList.addEventListener('click', supprCheck)
+myBtn.addEventListener('click', filterTodo)
 
 
